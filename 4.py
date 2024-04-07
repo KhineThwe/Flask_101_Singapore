a2875a3412bb3e836d76ba6389392982389bb297
khinezarthwe <khinezar70122107@gmail.com>
2024-04-07 13:07:29 +0900

    static files

---
+++ 4.py
@@ -0,0 +1,18 @@
+# from flask import Flask 
+
+# app = Flask(__name__) 
+
+# @app.route('/blog/<postID>') 
+# def show_blog(postID): 
+#     return 'Blog Number %s' % postID 
+
+# @app.route('/rev/<int:revNo>')  
+# def revision(revNo): 
+#     return 'Revision Number %d' % revNo 
+
+# @app.route('/rev/<float:revNo>')  
+# def revision1(revNo): 
+#     return 'Revision Number %f' % revNo
+
+# if __name__ == '__main__': 
+#     app.run() 
