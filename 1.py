db87d602f25ce853a960588b8b779b1e5ea0dcf5
khinezarthwe <khinezar70122107@gmail.com>
2024-05-02 20:23:10 +0900

    test

--- 1.py
+++ 1.py
@@ -16,19 +16,3 @@
 #     app.debug = True
 #     app.run() 
    
-import flask
-
-app = flask.Flask(__name__)
-
-
-class API:
-    def __init__(self):
-        app.add_url_rule("/", view_func=self.hello)
-
-    def hello(self):
-        return flask.Response("hello", 200)
-
-
-if __name__ == "__main__":
-    API()
-    app.run()
